4035e3ca43e6827c4dc45a7f053b7094e92b07de
Andre Pereira <andre.git07@gmail.com>
2020-05-10 16:39:56 -0300

    Projeto

---
+++ components/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className='py-4 text-center bg-gray-400'>
+      MyDailyStatus é um projeto criado durante o Fullstack Lab do DevPleno.
+      <br />
+      Implementado por:{' '}
+      <a href='https://github.com/andreps11'>André Pereira</a>
+    </div>
+  )
+}
+export default Footer
